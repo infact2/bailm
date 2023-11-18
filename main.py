41e71890db7d32049b640cfa7da268a4b009082b
JesseIguess <blueberry21007@gmail.com>
2023-11-18 09:29:57 +0000

    Added waitress for production

--- main.py
+++ main.py
@@ -114,4 +114,6 @@ def generate(starting_tokens):
     return output
     
 if __name__ == '__main__':
-   app.run(debug = True)
+#    app.run(debug = True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
